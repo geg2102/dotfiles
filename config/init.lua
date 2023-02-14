d8574f3613ee47db518cf437eeb1dd260e08a6f9
Geoffrey <Geoffrey_Grossman@brown.edu>
2023-02-13 20:06:12 -0500

    fix: scala remove augroup

--- config/init.lua
+++ config/init.lua
@@ -693,14 +693,7 @@ require("lazy").setup({
             metals_config.on_attach = function(client, bufnr)
                 require("metals").setup_dap()
             end
-            local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = { "scala", "sbt", "java" },
-                callback = function()
-                    require("metals").initialize_or_attach(metals_config)
-                end,
-                group = nvim_metals_group
-            })
+            require("metals").initialize_or_attach(metals_config)
         end
     },
     {
