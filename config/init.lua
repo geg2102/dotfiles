66db718f6e4739720c696f32454af2a4b0d447f1
Geoffrey <Geoffrey_Grossman@brown.edu>
2023-03-30 15:21:06 -0400

    feat: nvim-navbuddy

--- config/init.lua
+++ config/init.lua
@@ -78,6 +78,14 @@ require("lazy").setup({
         end
     },
     {
+        "SmiteshP/nvim-navbuddy", 
+        ft = { "python", "sh", "lua", "scala", "c", "cpp", "yaml", "json", "r" },
+        dependencies = {"neovim/nvim-lspconfig", "SmiteshP/nvim-navic", "MunifTanjim/nui.nvim"}, 
+        config = function()
+            require("nvim-navbuddy").setup{ window = { border = "double"}, lsp = { auto_attach = true}}
+        end
+    },
+    {
         "folke/trouble.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
         config = function()
