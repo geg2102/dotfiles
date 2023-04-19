0e9044b70f28bc335d4776df931e6bbc1212d46d
Geoffrey <Geoffrey_Grossman@brown.edu>
2023-04-19 10:45:05 -0400

    fix: comment check third party (don't know what it does)

--- config/init.lua
+++ config/init.lua
@@ -14,7 +14,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ","
-
 -- =====================================================================================
 -- PLUGINS
 -- =====================================================================================
@@ -187,7 +186,7 @@ require("lazy").setup({
                         },
                         workspace = {
                             library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false
+                            -- checkThirdParty = false
                         },
                         telemetry = {
                             enable = false,
