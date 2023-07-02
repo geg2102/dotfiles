48ce62c402d0fdae039aad75fba9dd8142c82491
Geoffrey Grossman <geoffrey@geoffreys-mbp.mynet>
2023-07-02 16:15:48 +0100

    fix: add keybindings for nvim-python-repl

--- config/init.lua
+++ config/init.lua
@@ -430,7 +430,16 @@ require("lazy").setup({
             require("nvim-python-repl").setup({
                 vsplit = false,
             })
-        end
+        end,
+        vim.keymap.set("n", "<leader>n", function() require('nvim-python-repl').send_statement_definition() end,
+            { desc = "Send semantic unit to REPL" }),
+        vim.keymap.set("v", "<leader>n", function() require('nvim-python-repl').send_visual_to_repl() end,
+            { desc = "Send visual selection to REPL" }),
+        vim.keymap.set("n", "<leader>nr", function() require('nvim-python-repl').send_buffer_to_repl() end,
+            { desc = "Send entire buffer to REPL" }),
+        vim.keymap.set("n", "<leader>e", function() require('nvim-python-repl').toggle_execute() end,
+            { desc = "Automatically execute command in REPL after sent" }),
+
     },
     {
         "beauwilliams/focus.nvim",
