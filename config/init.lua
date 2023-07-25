578af34c8825fb9db51cd6af0a0ea06a778512aa
Geoffrey Grossman <geoffrey@geoffreys-mbp.mynet>
2023-07-25 16:58:09 +0100

    feat: add markdown and markdown inline parsers

--- config/init.lua
+++ config/init.lua
@@ -76,7 +76,7 @@ require("lazy").setup({
     },
     {
         "SmiteshP/nvim-navic",
-        ft = { "python", "sh", "lua", "scala", "c", "cpp", "yaml", "json", "r" },
+        ft = { "python", "sh", "lua", "scala", "c", "cpp", "yaml", "json", "r", "markdown", "markdown_inline" },
         -- dependencies = "neovim/nvim-lspconfig",
         config = function()
             require("nvim-navic").setup()
