0e46ac560f579ddb0461d6e8d37c673bef39b565
Geoffrey Grossman <Geoffrey.Grossman@tdrcapital.com>
2023-11-17 09:30:46 +0000

    feat: shortcut for oil

--- config/init.lua
+++ config/init.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             require("oil").setup()
+            vim.keymap.set("n", "<leader>o", ":Oil --float<CR>", { silent=true, desc = "Send semantic unit to REPL" })
         end
     },
     {
