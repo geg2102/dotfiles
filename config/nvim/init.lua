38fad29fe7028bb2284cb2e945a4638a740dcd11
Geoffrey Grossman <Geoffrey.Grossman@tdrcapital.com>
2023-12-20 15:49:26 +0000

    feat: load kanagawa on clean nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,4 +2,12 @@ require("general")
 require("commands")
 if vim.g.noplugins == nil then
     require("plugins")
+else
+    local colors = vim.fn.stdpath("data") .. "/lazy/kanagawa.nvim/colors/kanagawa.vim"
+    if vim.fn.filereadable(colors) then
+        local kanagawa_dir = vim.fn.stdpath("data") .. "/lazy/kanagawa.nvim/lua"
+        local kanagawa_module_path = kanagawa_dir .. "/?.lua;" .. kanagawa_dir .. "/?/init.lua;"
+        package.path = kanagawa_module_path .. package.path
+        require("kanagawa").load()
+    end
 end
