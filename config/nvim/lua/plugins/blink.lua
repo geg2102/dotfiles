636783eb7ce5771bb17ed3b31e1cbf931f01aed1
Geoffrey Grossman <Geoffrey.Grossman@tdrcapital.com>
2025-01-03 11:42:17 +0000

    fix: c-space and c-e in blink

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -21,13 +21,15 @@ return {
             -- See the full "keymap" documentation for information on defining your own keymap.
             keymap = {
                 -- preset      = "default",
-                ["<C-y>"]   = { "select_and_accept" },
-                ["<Tab>"]   = { "select_next", "fallback" },
-                ["<S-Tab>"] = { "select_prev", "fallback" },
-                ["<C-p>"]   = { "snippet_backward" },
-                ["<C-n>"]   = { "snippet_forward" },
-                ['<C-b>']   = { 'scroll_documentation_up', 'fallback' },
-                ['<C-f>']   = { 'scroll_documentation_down', 'fallback' },
+                ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+                ['<C-e>']     = { 'hide' },
+                ["<C-y>"]     = { "select_and_accept" },
+                ["<Tab>"]     = { "select_next", "fallback" },
+                ["<S-Tab>"]   = { "select_prev", "fallback" },
+                ["<C-p>"]     = { "snippet_backward" },
+                ["<C-n>"]     = { "snippet_forward" },
+                ['<C-b>']     = { 'scroll_documentation_up', 'fallback' },
+                ['<C-f>']     = { 'scroll_documentation_down', 'fallback' },
             },
             -- keymap = { preset = 'default' },
             sources = {
