225dc2867909135df9c21ab97c269edc83e0b709
Geoffrey Grossman <Geoffrey.Grossman@tdrcapital.com>
2025-03-11 14:38:03 +0000

    fix: mapping for cmp in dbee

--- config/nvim/lua/plugins/dbee.lua
+++ config/nvim/lua/plugins/dbee.lua
@@ -36,7 +36,7 @@ return {
                 opts = {},  -- needed
             },
         },
-        ft = { "sql" },
+        ft = "sql",
         -- event = "InsertEnter",
         config = function()
             local cmp = require("cmp")
@@ -64,24 +64,28 @@ return {
                     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
                     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
                     ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif require("luasnip").expand_or_locally_jumpable() then
-                            require("luasnip").expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif require("luasnip").locally_jumpable(-1) then
-                            require("luasnip").jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
+                    -- ["<Down>"] = cmp.mapping(function(fallback)
+                    --     if cmp.visible() then
+                    --         cmp.select_next_item()
+                    --     elseif require("luasnip").expand_or_locally_jumpable() then
+                    --         require("luasnip").expand_or_jump()
+                    --     else
+                    --         fallback()
+                    --     end
+                    -- end, { "i", "s" }),
+                    -- ["<Up>"] = cmp.mapping(function(fallback)
+                    --     if cmp.visible() then
+                    --         cmp.select_prev_item()
+                    --     elseif require("luasnip").locally_jumpable(-1) then
+                    --         require("luasnip").jump(-1)
+                    --     else
+                    --         fallback()
+                    --     end
+                    -- end, { "i", "s" }),
+                    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                        { 'i' }),
+                    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+                        { 'i' }),
                 },
                 formatting = {
                     format = require("lspkind").cmp_format({
